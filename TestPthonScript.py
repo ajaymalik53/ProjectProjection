d869ec2029f4865b6a35ecef408d8422c3e43f54
ajaymalik53 <39063081+ajaymalik53@users.noreply.github.com>
2019-12-09 16:41:01 +0530

    Update TestPthonScript.py

--- TestPthonScript.py
+++ TestPthonScript.py
@@ -2,3 +2,11 @@ import pandas as pd
 from datetime import datetime
 
 data = pd.read_excel('Final.xlsx')
+primeCustomerData = pd.read_excel('PrimeCustomerDetails.xlsx')
+primeCustomer = primeCustomerData.iloc[0,0]
+planDate = primeCustomerData.iloc[0,1]
+customerCode = customerRanking['Customer Code'].tolist()
+customerRank = customerRanking['Rank'].tolist()
+data.insert(5,'Week_And_Year',str)
+data.insert(9,'PrimeAndOtherCustomerRanks',int)
+data.insert(10,'CustomerRanking',int)
